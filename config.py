e3224c310a40e81fbc4911f21187c829448b0c27
Zaixin Ou <61226078+Ouzaixin@users.noreply.github.com>
2024-08-24 23:05:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 learning_rate = 1e-4
 batch_size = 1
 numworker = 0
-epochs = 40
+epochs = 1000
+latent_dim = 1728
+num_clusters = 3
 gpus = [0]
 
 whole_MRI = "./data/whole_MRI"
@@ -13,7 +15,8 @@ whole_AV45 = "./data/whole_AV45"
 whole_Tau = "./data/whole_Tau"
 
 exp = "exp_1/"
-whole = "./data_info/whole.txt"
-num_clusters = 3
+train_file = "./data_info/train.txt"
+validation_file = "./data_info/validation.txt"
+test_file = "./data_info/test.txt"
 
 CHECKPOINT_model = "result/"+exp+"model.pth.tar"
