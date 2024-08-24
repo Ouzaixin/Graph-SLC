9f750978bdae31a2480b51ae43f66b22eec88cd7
Zaixin Ou <61226078+Ouzaixin@users.noreply.github.com>
2024-08-24 23:05:44 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -17,68 +17,28 @@ def nifti_to_numpy(file):
     data = data.astype(np.float32)
     return data
 
-def train_split(file,fold,name):
-    file=open(file,"r")
-    S=file.read().split()
-    p=list(str(i) for i in S)
-    x=int(len(p)/5)
-    if name == "train":
-        if fold == "0":
-            p = p[:x*3]
-        elif fold == "1":
-            p = p[x:x*4]
-        elif fold == "2":
-            p = p[x*2:]
-        elif fold == "3":
-            p = p[x*3:]+p[:x]
-        elif fold == "4":
-            p = p[x*4:]+p[:x*2]
-    elif name == "validation":
-        if fold == "0":
-            p = p[x*3:x*4]
-        elif fold == "1":
-            p = p[x*4:]
-        elif fold == "2":
-            p = p[:x]
-        elif fold == "3":
-            p = p[x:x*2]
-        elif fold == "4":
-            p = p[x*2:x*3]
-    elif name == "test":
-        if fold == "0":
-            p = p[x*4:]
-        elif fold == "1":
-            p = p[:x]
-        elif fold == "2":
-            p = p[x:x*2]
-        elif fold == "3":
-            p = p[x*2:x*3]
-        elif fold == "4":
-            p = p[x*3:x*4]
-    return p
-
 class OneDataset(Dataset):
-    def __init__(self, task = config.whole, name = "train", fold = "0"):
+    def __init__(self, file = config.train_file, name = "train"):
         self.name = name
-        self.MRI = train_split(task,fold,name)
-        self.length_dataset = len(self.MRI)
-        self.data = pd.read_csv("data_info/info.csv",encoding = "ISO-8859-1")
+        self.file_name = read_list(file)
+        self.length_dataset = len(self.file_name)
+        # self.data = pd.read_csv("data_info/info.csv",encoding = "ISO-8859-1")
 
     def __len__(self):
         return self.length_dataset
 
     def __getitem__(self, index):
-        MRI_name = self.MRI[index % self.length_dataset] + ".nii"
-        MRI_path = os.path.join(config.whole_MRI, MRI_name)
-        MRI = nifti_to_numpy(MRI_path)
+        name = self.file_name[index % self.length_dataset] + ".nii"
+        # MRI_path = os.path.join(config.whole_MRI, MRI_name)
+        # MRI = nifti_to_numpy(MRI_path) if os.path.exists(MRI_path) else np.zeros((1, 1))
 
-        FDG_path = os.path.join(config.whole_FDG, MRI_name)
-        FDG = nifti_to_numpy(FDG_path) if os.path.exists(FDG_path) else np.zeros((1, 1))
+        # FDG_path = os.path.join(config.whole_FDG, MRI_name)
+        # FDG = nifti_to_numpy(FDG_path) if os.path.exists(FDG_path) else np.zeros((1, 1))
 
-        AV45_path = os.path.join(config.whole_AV45, MRI_name)
-        AV45 = nifti_to_numpy(AV45_path) if os.path.exists(AV45_path) else np.zeros((1, 1))
+        # AV45_path = os.path.join(config.whole_AV45, MRI_name)
+        # AV45 = nifti_to_numpy(AV45_path) if os.path.exists(AV45_path) else np.zeros((1, 1))
 
-        Tau_path = os.path.join(config.whole_Tau, MRI_name)
-        Tau = nifti_to_numpy(Tau_path) if os.path.exists(Tau_path) else np.zeros((1, 1))
+        # Tau_path = os.path.join(config.whole_Tau, MRI_name)
+        # Tau = nifti_to_numpy(Tau_path) if os.path.exists(Tau_path) else np.zeros((1, 1))
 
-        return MRI, FDG, AV45, Tau, MRI_name
+        return name
